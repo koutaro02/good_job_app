c6dbf2f781f8e7434da9c82c9e1cff402d4bf37c
koutaro02 <k.u89yakyuu@icloud.com>
2021-09-08 00:49:58 +0900

    mapsテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_05_082855) do
+ActiveRecord::Schema.define(version: 2021_09_07_154823) do
+
+  create_table "maps", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "address"
+    t.float "latitude"
+    t.float "longitude"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "family_name", null: false
