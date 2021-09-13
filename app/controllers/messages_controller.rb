d278e8b1fe920c4f545b76809c140b338e471b89
koutaro02 <k.u89yakyuu@icloud.com>
2021-09-13 16:13:42 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,23 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    @map = Map.find(params[:map_id])
+  end
+
+  def create
+    @map = Map.find(params[:map_id])
+    @message = @map.messages.new(message_params)
+    if @message.save
+      redirect_to map_messages_path(@map)
+    else
+      @messages = @map.messages.includes(:user)
+      render :index
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:comment,:image).merge(user_id: current_user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "maps#index"
   resources :users, onry: [:edit, :update]
-  resources :maps, onry: [:new, :create]
+  resources :maps, onry: [:new, :create] do
+    resources :messages, onry: [:index, :create]
+  end
 end
