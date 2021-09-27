2c0cb0f606f2a7da26ab42fef63bbf4e0141b0dd
koutaro02 <k.u89yakyuu@icloud.com>
2021-09-27 15:53:13 +0900

    各モデルのテスト

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    before do
+      @message = FactoryBot.build(:message)
+    end
+
+  describe 'メッセージの登録' do
+    context '登録できる時' do
+
+      it 'commentとimageが存在していれば保存できること' do
+        expect(@message).to be_valid
+      end
+
+      it 'commentが空でも保存できること' do
+        @message.comment = nil
+        expect(@message).to be_valid
+      end
+
+      it 'imageが空でも保存できること' do
+        @message.image = nil
+        expect(@message).to be_valid
+      end
+
+    end
+
+    context '登録できない時' do
+
+      it 'commentとimageが空では保存できないこと' do
+        @message.comment = nil
+        @message.image = nil
+        @message.valid?
+        expect(@message.errors.full_messages).to include("Comment can't be blank")
+      end
+
+      it 'mapが紐付いていないと保存できないこと' do
+        @message.map = nil
+        @message.valid?
+        expect(@message.errors.full_messages).to include("Map must exist")
+      end
+
+      it 'userが紐付いていないと保存できないこと' do
+        @message.user = nil
+        @message.valid?
+        expect(@message.errors.full_messages).to include("User must exist")
+      end
+
+    end
+  end
 end
